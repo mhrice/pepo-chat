0b8d228cddfb59e37d7d1c6a4296c6e44d25c551
mhrice <matthewrice11@yahoo.com>
2017-10-22 02:25:12 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').load();
+var cors = require('cors')
+// Node/Express
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const router = require('./src/router');
+
+// Create Express webapp
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Add body parser for Notify device registration
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(router);
+app.use(cors());
+
+// Create http server and run it
+const server = http.createServer(app);
+const port = process.env.PORT || 3002;
+server.listen(port, function() {
+  console.log('Express server running on *:' + port);
+});
+
+module.exports = app;
